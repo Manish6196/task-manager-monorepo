e6457f493446b266284f38f37d3467a7e9c65f32
manish <manishrajput6196@gmail.com>
2024-03-23 23:30:11 +0530

    express-api: add get and post endpoints

--- apps/express-api/src/main.ts
+++ apps/express-api/src/main.ts
@@ -14,6 +14,24 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to express-api!' });
 });
 
+interface Todo {
+  title: string;
+}
+
+const todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+app.get('/api/todos', (req, res) => {
+  res.send(todos);
+});
+
+app.post('/api/addTodo', (req, res) => {
+  const newTodo = {
+    title: `New todo ${Math.floor(Math.random() * 1000)}`,
+  };
+  todos.push(newTodo);
+  res.send(newTodo);
+});
+
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
