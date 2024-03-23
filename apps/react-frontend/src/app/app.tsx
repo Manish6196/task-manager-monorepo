6d82c71167222c581197c48ba8faa5d32f4a8a4d
manish <manishrajput6196@gmail.com>
2024-03-23 21:20:28 +0530

    react-frontend: add data fetching loginc

--- apps/react-frontend/src/app/app.tsx
+++ apps/react-frontend/src/app/app.tsx
@@ -1,23 +1,48 @@
-import { useEffect } from 'react';
-import NxWelcome from './nx-welcome';
+import { useEffect, useState } from 'react';
+
+interface Todo {
+  title: string;
+}
 
 export function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   useEffect(() => {
     (async () => {
       try {
         const res = await fetch('/api/todos');
-        const data = await res.json();
-        console.log(data);
+        const todos = await res.json();
+        setTodos(todos);
       } catch (e: any) {
         console.error(e.message);
       }
     })();
   }, []);
 
+  const addTodo = async () => {
+    try {
+      const res = await fetch('/api/addTodo', { method: 'POST', body: '' });
+      const newTodo = await res.json();
+      setTodos([...todos, newTodo]);
+    } catch (e: any) {
+      console.error(e.message);
+    }
+  };
+
   return (
-    <div>
-      <NxWelcome title="react-frontend" />
-    </div>
+    <>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((t) => (
+          <li className="todo" key={t.title}>
+            {t.title}
+          </li>
+        ))}
+      </ul>
+      <button id="add-todo" onClick={addTodo}>
+        Add Todo
+      </button>
+    </>
   );
 }
 
