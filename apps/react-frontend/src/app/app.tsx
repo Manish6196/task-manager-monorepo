7fcd7fe8201b39d9a66676657c0e66f56f9bfc43
manish <manishrajput6196@gmail.com>
2024-03-23 20:30:07 +0530

    react frontend: add api proxy

--- apps/react-frontend/src/app/app.tsx
+++ apps/react-frontend/src/app/app.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch('/api');
+        const data = await res.json();
+        console.log(data);
+      } catch (e: any) {
+        console.error(e.message);
+      }
+    })();
+  }, []);
+
   return (
     <div>
       <NxWelcome title="react-frontend" />
